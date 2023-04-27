440f16459f6470e504f4a51a829c3b5a85078397
hasdsd <1259305706@qq.com>
2023-04-27 16:48:24 +0800

    教师完成

--- src/pages/teacher.vue
+++ src/pages/teacher.vue
@@ -1,18 +1,22 @@
 <template>
     <div class="q-pa-md">
         <!--按钮-->
+        <!--按钮-->
         <div class="col q-mb-md">
             <div class="row justify-between">
                 <div class="col">
-                    <q-btn color="primary" class="q-mr-md" label="刷新" icon="refresh"/>
-                    <q-btn color="secondary" class="q-mr-md" label="新增" icon="add"/>
-                    <q-btn color="red" class="q-mr-md" label="删除" icon="delete"/>
+                    <q-btn color="primary" class="q-mr-md" label="刷新" icon="refresh"
+                           @click="refresh"/>
+                    <q-btn color="secondary" class="q-mr-md" label="新增" icon="add" @click="addDialog=true"/>
+                    <q-btn color="red" class="q-mr-md" label="删除" icon="delete" @click="handleDelete"/>
                 </div>
                 <div class="col text-right">
                     <q-input filled dense v-model="searchName" label="姓名" class="inline-block q-mr-sm"/>
-                    <q-input filled dense v-model="searchNumber" label="工号" class="inline-block q-mr-sm"/>
+                    <q-input filled dense v-model="searchNumber" label="学号" class="inline-block q-mr-sm"/>
                     <q-input filled dense v-model="searchClass" label="学院" class="inline-block q-mr-sm"/>
-                    <q-btn color="primary" class="inline vertical-top" label="搜索" icon="search"/>
+                    <q-btn color="red" class="inline vertical-top q-mr-sm" label="重置" icon="restart_alt"
+                           @click="resetSearch"/>
+                    <q-btn color="primary" class="inline vertical-top" label="搜索" icon="search" @click="loadPage"/>
                 </div>
             </div>
         </div>
@@ -20,9 +24,9 @@
         <div>
             <q-table title="导员管理"
                      :rows="studentList"
-                     :columns="columns"
+                     :columns="teacherColumns"
                      row-key="name"
-                     :pagination="{rowsNumber:pageSize}"
+                     :pagination="{rowsNumber:page.pageSize}"
                      selection="multiple"
                      v-model:selected="selected"
                      hide-pagination>
@@ -46,46 +50,99 @@
             </q-table>
             <div class="q-pa-lg flex flex-center">
                 <q-pagination
-                        v-model="currentPage"
-                        :max="totalPage"
+                        v-model="page.currentPage"
+                        :max="page.totalPage"
+                        @update:model-value="loadPage"
                         direction-links
                 />
             </div>
         </div>
+        <!--    新增窗口    -->
+        <q-dialog v-model="addDialog" position="right" full-height>
+            <AddDialog :info="info" :column="teacherColumns"/>
+        </q-dialog>
     </div>
 </template>
 <script setup lang="ts">
+
 import {api} from 'src/boot/axios';
 import {ref} from 'vue';
+import {Page} from "components/entity";
+import {CommonLoading, CommonSuccess, LoadingFinish} from "components/commonResults";
+import {useQuasar} from "quasar";
+import AddDialog from "components/AddDialog.vue";
 
+const $q = useQuasar()
+//分页管理
+const teacherColumns: any = [
+    {name: 'id', align: 'center', label: 'id', 'field': 'id', 'type': 'input'},
+    {name: 'name', align: 'center', label: '姓名', 'field': 'name', 'type': 'input'},
+    {name: 'number', align: 'center', label: '工号', 'field': 'number', 'type': 'number'},
+    {name: 'collegeId', align: 'center', label: '学院', 'field': 'collegeId', 'type': 'number'},
+    {name: 'role', align: 'center', label: '角色', 'field': 'role', 'type': 'number'},
+    {name: 'enable', align: 'center', label: '账号状态', 'field': 'enable', 'type': 'bool'},
+    {name: 'handle', align: 'center', label: '操作', 'field': 'handle'},
+]
+const page = ref(new Page(1, 20, 21,))
 
 //加载表格
 const studentList = ref([])
 const selected = ref([])
-loadPage()
-
-function loadPage() {
-    api.get("/admin?currentPage=1&pageSize=10").then(res => {
-        studentList.value = res.data
-    })
-}
-
-//分页管理
-const currentPage = ref(1)
-const pageSize = ref(20)
-const totalPage = ref(5)
-const columns: any = [
-    {name: 'id', align: 'center', label: '学生id', 'field': 'id'},
-    {name: 'name', align: 'center', label: '姓名', 'field': 'name'},
-    {name: 'number', align: 'center', label: '学号', 'field': 'number'},
-    {name: 'collegeId', align: 'center', label: '学院id', 'field': 'collegeId'},
-    {name: 'role', align: 'center', label: '角色', 'field': 'role'},
-    {name: 'enable', align: 'center', label: '账号状态', 'field': 'enable'},
-    {name: 'handle', align: 'center', label: '操作', 'field': 'handle'},
-]
-
-//搜索
 const searchName = ref('')
 const searchNumber = ref('')
 const searchClass = ref([])
+
+loadPage()
+
+function loadPage() {
+    CommonLoading($q)
+    api.get("/admin", {
+        params: {
+            'currentPage': page.value.currentPage,
+            'pageSize': page.value.pageSize,
+            'username': searchName.value,
+            'number': searchNumber.value,
+            'classId': searchClass.value
+        }
+    }).then(res => {
+        studentList.value = res.data
+        LoadingFinish($q)
+    })
+}
+
+//刷新表格
+function refresh() {
+    loadPage()
+    CommonSuccess('刷新完成')
+}
+
+//重置搜索
+function resetSearch() {
+    searchName.value = ''
+    searchNumber.value = ''
+    searchClass.value = []
+    page.value.currentPage = 1
+    loadPage()
+}
+
+//新增用户
+const addDialog = ref(false)
+const info = ref({title: '新增用户'})
+// const data = ref('aaa')
+
+//删除用户
+function handleDelete() {
+    $q.dialog({
+        title: '删除用户',
+        message: '你确定要删除吗?',
+        cancel: true
+    }).onOk(() => {
+        selected.value.forEach((item: any) => {
+            console.log(item.studentId)
+        })
+        loadPage()
+    })
+}
+
+
 </script>
