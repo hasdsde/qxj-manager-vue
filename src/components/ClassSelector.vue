72d803d1df0d8845f757aadaaf58a51c054c8624
hasdsd <1259305706@qq.com>
2023-05-05 16:36:00 +0800

    班级选择器

--- src/components/ClassSelector.vue
+++ src/components/ClassSelector.vue
@@ -7,15 +7,18 @@
     </q-input>
 
     <q-dialog v-model="dialog">
-        <q-card>
+        <q-card style="width: 300px">
             <q-card-section>
                 <div class="text-h6">班级选择器</div>
             </q-card-section>
 
             <q-card-section class="q-pt-none">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas
-                eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus
-                minima, porro labore.
+                <q-select class="q-mt-md" filled v-model="college" :options="colleges" label="学院"
+                          @update:model-value="onCollegeUpdate"/>
+                <q-select class="q-mt-md" filled v-model="grade" :options="grades" label="年级"/>
+                <q-select class="q-mt-md" filled v-model="major" :options="majors" label="专业"
+                          @update:model-value="onClassUpdate"/>
+                <q-select class="q-mt-md" filled v-model="classe" :options="classes" label="班级"/>
             </q-card-section>
 
             <q-card-actions align="right">
@@ -28,7 +31,66 @@
 <script lang="ts" setup>
 
 import {ref} from "vue";
+import {api} from "boot/axios";
 
 const class_id = ref()
 const dialog = ref(false)
+const colleges = ref([])
+const college: any = ref()
+const grades = ref([])
+const grade: any = ref([])
+const majors = ref([])
+const major = ref()
+const classes = ref([])
+const classe = ref()
+getColleges()
+
+function getColleges() {
+    api.get('/class/college').then((res: any) => {
+        res.data.forEach((item: any) => {
+            item.label = item.name
+        })
+        colleges.value = res.data
+        console.log(colleges)
+    })
+}
+
+function onCollegeUpdate() {
+    console.log(college)
+    getMajorId()
+    getGradeId()
+}
+
+function getMajorId() {
+    api.get('/class/major?collegeId=' + college.value.id).then((res: any) => {
+        res.data.forEach((item: any) => {
+            item.label = item.name
+        })
+        majors.value = res.data
+    })
+}
+
+function getGradeId() {
+    api.get('/class/grade?collegeId=' + college.value.id).then((res: any) => {
+        res.data.forEach((item: any) => {
+            item.label = item.name
+        })
+        grades.value = res.data
+    })
+}
+
+function onClassUpdate() {
+    api.get('/class', {
+        params: {
+            'majorId': major.value.id,
+            'gradeId': grade.value.id
+        }
+    }).then((res: any) => {
+        res.data.forEach((item: any) => {
+            item.label = item.name
+        })
+        console.log(res.data)
+        classes.value = res.data
+    })
+}
 </script>
