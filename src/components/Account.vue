765c5ec3c11cccea6a026745a2ccbc0c40ae40a7
hasdsd <1259305706@qq.com>
2023-05-07 15:01:13 +0800

    登录做了一半

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,75 +1,83 @@
 <template>
-  <q-avatar class="q-mr-sm">
-    <!-- <img src="https://cdn.quasar.dev/img/avatar.png"> -->
-  </q-avatar>
-  <div class="q-mr-sm">{{ '用户： ' + userName }}</div>
-  <q-btn color="secondary" size="sm" label="选项">
-    <q-menu>
-      <q-list style="min-width: 100px">
-        <q-item clickable v-close-popup>
-          <q-item-section @click="updatePassword">修改密码</q-item-section>
-        </q-item>
-        <q-item clickable v-close-popup>
-          <q-item-section @click="logout">退出</q-item-section>
-        </q-item>
-        <q-separator />
-      </q-list>
-    </q-menu>
-  </q-btn>
+    <q-avatar class="q-mr-sm">
+        <!-- <img src="https://cdn.quasar.dev/img/avatar.png"> -->
+    </q-avatar>
+    <div class="q-mr-sm">{{ '用户： ' + userInfo.name }}</div>
+    <q-btn color="secondary" size="sm" label="选项">
+        <q-menu>
+            <q-list style="min-width: 100px">
+                <q-item clickable v-close-popup>
+                    <q-item-section @click="updatePassword">修改密码</q-item-section>
+                </q-item>
+                <q-item clickable v-close-popup>
+                    <q-item-section @click="logout">退出</q-item-section>
+                </q-item>
+                <q-separator/>
+            </q-list>
+        </q-menu>
+    </q-btn>
 
   <!-- 修改密码对话框 -->
-  <q-dialog v-model="dialog">
-    <q-card style="min-width: 20%;">
-      <q-card-section class="row items-center q-pb-none">
-        <div class="text-h6">修改密码</div>
-        <q-space />
-        <q-btn icon="close" flat round dense v-close-popup />
-      </q-card-section>
-      <q-card-section>
-        <q-form class="q-gutter-md">
-          <q-input class="q-pa-sm" filled v-model="old_pwd" :rules="[val => val && val.length > 0 || '必填']" label="旧密码" />
-          <q-input class="q-pa-sm" filled v-model="new_pwd" :rules="[value => value && value.length > 0 || '必填']"
-            label="新密码" />
-          <q-input class="q-pa-sm" filled v-model="confirm_pwd"
-            :rules="[val => val && val.length > 0 || '必填', value => value === new_pwd || '旧密码与新密码不一致']" label="确认密码" />
-        </q-form>
-      </q-card-section>
-      <q-card-actions align="right">
-        <q-btn flat label="重置" type="reset" color="primary" @click="onReset" />
-        <q-btn flat label="提交" type="submit" color="red" @click="onSubmit" />
-      </q-card-actions>
-    </q-card>
-  </q-dialog>
+    <q-dialog v-model="dialog">
+        <q-card style="min-width: 20%;">
+            <q-card-section class="row items-center q-pb-none">
+                <div class="text-h6">修改密码</div>
+                <q-space/>
+                <q-btn icon="close" flat round dense v-close-popup/>
+            </q-card-section>
+            <q-card-section>
+                <q-form class="q-gutter-md">
+                    <q-input class="q-pa-sm" filled v-model="old_pwd" :rules="[val => val && val.length > 0 || '必填']"
+                             label="旧密码"/>
+                    <q-input class="q-pa-sm" filled v-model="new_pwd"
+                             :rules="[value => value && value.length > 0 || '必填']"
+                             label="新密码"/>
+                    <q-input class="q-pa-sm" filled v-model="confirm_pwd"
+                             :rules="[val => val && val.length > 0 || '必填', value => value === new_pwd || '旧密码与新密码不一致']"
+                             label="确认密码"/>
+                </q-form>
+            </q-card-section>
+            <q-card-actions align="right">
+                <q-btn flat label="重置" type="reset" color="primary" @click="onReset"/>
+                <q-btn flat label="提交" type="submit" color="red" @click="onSubmit"/>
+            </q-card-actions>
+        </q-card>
+    </q-dialog>
 </template>
 <script setup lang="ts">
-import { CommonFail, CommonSuccess, CommonWarn } from 'src/components/commonResults'
-import { useQuasar } from 'quasar';
-import { ref, watch } from 'vue';
-import { useRouter } from "vue-router";
-import { api } from 'src/boot/axios';
+import {CommonFail, CommonWarn, Success} from 'src/components/commonResults'
+import {useQuasar} from 'quasar';
+import {ref} from 'vue';
+import {api} from 'src/boot/axios';
+import {useRouter} from "vue-router";
 
 const $q = useQuasar()
-let rt = useRouter()
 let dialog = ref(false)
+const rt = useRouter()
+const userInfo: any = ref({})
+getUserInfo()
 
-// 获取用户名
-const userName = ref('未登录')
-getUsername()
-function getUsername() {
-  return localStorage.getItem("nickname") || localStorage.getItem("username");
+function getUserInfo() {
+    if (localStorage.getItem("userInfo") == null) {
+        CommonFail('用户未登录')
+        rt.push('/Login')
+    } else {
+        //@ts-ignore
+        userInfo.value = JSON.parse(localStorage.getItem("userInfo").toString())
+    }
 }
 
 // 登出
 function logout() {
-  $q.dialog({
-    title: '登出',
-    message: '你确定要登出吗？',
-    cancel: true,
-    persistent: true
-  }).onOk(() => {
-    window.location.href = '/#/login'
-    localStorage.clear()
-  })
+    $q.dialog({
+        title: '登出',
+        message: '你确定要登出吗？',
+        cancel: true,
+        persistent: true
+    }).onOk(() => {
+        window.location.href = '/#/login'
+        localStorage.clear()
+    })
 }
 
 // 修改密码
@@ -78,39 +86,38 @@ let new_pwd = ref('');
 let confirm_pwd = ref('');
 
 function onSubmit() {
-  if (!old_pwd.value || !new_pwd.value || !confirm_pwd.value) {
-    CommonWarn("提示：请输入正确的信息");
-    return;
-  } else if (new_pwd.value !== confirm_pwd.value) {
-    CommonWarn('两次信息不一致');
-    return;
-  } else {
-    api.put('/user/password', null, {
-      params: {
-        oldPassword: old_pwd.value,
-        newPassword: new_pwd.value
-      }
-    }).then(res => {
-      CommonSuccess();
-    }).catch(res => {
-      CommonFail();
-    })
-    dialog.value = false
-    onReset()
-  }
+    if (!old_pwd.value || !new_pwd.value || !confirm_pwd.value) {
+        CommonWarn("提示：请输入正确的信息");
+        return;
+    } else if (new_pwd.value !== confirm_pwd.value) {
+        CommonWarn('两次信息不一致');
+        return;
+    } else {
+        api.put('/user/password', null, {
+            params: {
+                oldPassword: old_pwd.value,
+                newPassword: new_pwd.value
+            }
+        }).then(res => {
+            Success();
+        }).catch(res => {
+            CommonFail('失败');
+        })
+        dialog.value = false
+        onReset()
+    }
 }
 
 function onReset() {
-  old_pwd.value = '';
-  new_pwd.value = '';
-  confirm_pwd.value = '';
+    old_pwd.value = '';
+    new_pwd.value = '';
+    confirm_pwd.value = '';
 }
 
 //修改密码弹窗
 function updatePassword() {
-  dialog.value = true
+    dialog.value = true
 }
-
 
 
 </script>
--- src/layouts/Login.vue
+++ src/layouts/Login.vue
@@ -1,53 +1,45 @@
 <template>
-  <div class="q-pa-md main" style="height: 100vh;">
-    <!--   背景图片   -->
-    <!--    <BackgroundImg />-->
-    <div class="column justify-center" style="height: 90vh;">
-      <div class="col-auto row justify-center">
-        <q-card class="col-auto shadow-10" style="background-color: rgb(255,255,255);padding: 50px;width: 400px">
-          <q-card-section class="row justify-between">
-            <strong>{{ pageTitle }}</strong>
-            <q-btn icon="close" dense rounded flat to="/" />
-          </q-card-section>
+    <div class="q-pa-md main" style="height: 100vh;">
+        <!--   背景图片   -->
+        <!--    <BackgroundImg />-->
+        <div class="column justify-center" style="height: 90vh;">
+            <div class="col-auto row justify-center">
+                <q-card class="col-auto shadow-10"
+                        style="background-color: rgb(255,255,255);padding: 50px;width: 400px">
+                    <q-card-section class="row justify-between">
+                        <strong>登录</strong>
+                        <q-btn icon="close" dense rounded flat to="/"/>
+                    </q-card-section>
 
-          <!--     用户名     -->
-          <q-card-section>
-            <q-input rounded ref="usernameRef" v-model="usernameR" label="用户名" :lazy-rules="true"
-              :rules="[(val) => (val.length > 0) || '输入值为空']" />
-          </q-card-section>
+                    <!--     学号     -->
+                    <q-card-section>
+                        <q-input rounded ref="usernameRef" v-model="number" label="学号/工号" :lazy-rules="true"
+                                 :rules="[(val) => (val.length > 0) || '输入值为空']"/>
+                    </q-card-section>
 
-          <!--     密码     -->
-          <q-card-section>
-            <q-input rounded ref="passwordRef" v-model="passwordR" label="密码" lazy-rules
-              :rules="[(val) => (val && val.length > 0) || '输入值为空']" type="password" />
-          </q-card-section>
+                    <!--     密码     -->
+                    <q-card-section>
+                        <q-input rounded ref="passwordRef" v-model="password" label="密码" lazy-rules
+                                 :rules="[(val) => (val && val.length > 0) || '输入值为空']" type="password"/>
+                    </q-card-section>
 
-          <!--     确认密码     -->
-          <q-slide-transition>
-            <q-card-section v-if="registerR">
-              <q-input rounded v-model="conformR" ref="passwordConformRef" label="确认密码" lazy-rules
-                :rules="[(val) => (val && val === passwordR && val.length > 0) || '两次输入密码不一致']" type="password" />
-            </q-card-section>
-          </q-slide-transition>
-
-          <q-card-section>
-            <q-btn-group>
-              <q-btn @click="handlerLogin" color="primary" style="width: 205px;" :label="second" />
-              <q-btn @click="registerR = !registerR; switchLabel()" color="secondary" :label="first" />
-            </q-btn-group>
-          </q-card-section>
-        </q-card>
-      </div>
+                    <q-card-section>
+                        <q-btn-group>
+                            <q-btn @click="handleLogin" color="primary" style="width: 205px;" label="登录"/>
+                            <q-btn @click="handleRegister" color="secondary" label="注册"/>
+                        </q-btn-group>
+                    </q-card-section>
+                </q-card>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
-<script setup>
-// import BackgroundImg from "../components/BackgroundImg"
-import { ref } from 'vue'
-import { api } from '../boot/axios'
-import { CommonFail, CommonSuccess } from '../components/commonResults'
-import { useRouter } from 'vue-router'
+<script setup lang="ts">
+import {ref} from 'vue'
+import {api} from 'boot/axios'
+import {CommonFail, CommonSuccess} from 'components/commonResults'
+import {useRouter} from 'vue-router'
 
 
 const $router = useRouter()
@@ -57,90 +49,54 @@ const usernameRef = ref(null)
 const passwordRef = ref(null)
 const passwordConformRef = ref(null)
 
-const nickname = ref('NTR');
 
-const pageTitle = ref('登录')
-const usernameR = ref('')
-const passwordR = ref('')
-const conformR = ref()
-const registerR = ref(false)
-const first = ref('注册')
-const second = ref('登录')
+const number = ref('')
+const password = ref('')
+const registerMode = ref(false)
 
-
-// 登录和注册切换
-function switchLabel() {
-  const temp = first.value
-  first.value = second.value
-  second.value = temp
-  passwordR.value = ''
-  conformR.value = ''
-}
 
 // 重置输入框
 function clearAll() {
-  usernameR.value = ''
-  passwordR.value = ''
-  conformR.value = ''
+    number.value = ''
+    password.value = ''
+
 }
 
-// 点击右侧 登录/注册 按钮
-function handlerLogin() {
-  localStorage.clear()
-  if (second.value === '登录' && loginRule()) { // 登录
-    login()
-  } else if (second.value === '注册' && regRule()) { // 注册
-    register()
-  }
-}
 
 // 注册
-function register() {
-  api.post('/user/register', {
-    'username': usernameR.value,
-    'password': passwordR.value
-  }).then(res => {
-    if (res.data.code === '200') {
-      CommonSuccess('注册成功')
-      login()
-      clearAll()
-    } else
-      CommonFail('注册失败')
-  })
+function handleRegister() {
+    api.post('/admin/login', {
+        'number': number.value,
+        'password': password.value
+    }).then(res => {
+        if (res.data.code === '200') {
+            CommonSuccess('注册成功')
+            handleLogin()
+            clearAll()
+        } else
+            CommonFail('注册失败')
+    })
 }
 
 //登录
-async function login() {
-  await api.get('/user/login', {
-    params: {
-      'username': usernameR.value,
-      'password': passwordR.value
-    }
-  }).then(res => {
-
-    if (res.data.code === '200') {
-      localStorage.setItem('username', usernameR.value)
-      localStorage.setItem('nickname', res.data.data.user.nickname)
-      localStorage.setItem('userId', user.id)
-      localStorage.setItem('jwt_token', res.data.data.token)
-
-      CommonSuccess('登录成功')
-      $router.push('/')
-
-    } else {
-      CommonFail('登录失败')
-    }
-  })
+async function handleLogin() {
+    await api.post('/admin/login', {
+        'number': number.value,
+        'password': password.value
+    }).then((res: any) => {
+        if (res.code == '200') {
+            localStorage.setItem('userInfo', JSON.stringify(res.data))
+            CommonSuccess('登录成功')
+            $router.push('/')
+        } else {
+            CommonFail('登录失败')
+        }
+    })
 }
 
 // 登录请求限制
 function loginRule() {
-  return !(usernameR.value === '' || passwordR.value === '')
-}
-
-// 注册请求限制
-function regRule() {
-  return (loginRule() && passwordR.value === conformR.value)
+    return !(number.value === '' || password.value === '')
 }
 
 
@@ -149,7 +105,7 @@ function regRule() {
 
 <style scoped>
 .main {
-  background-image: url('../assets/kiminame.png');
-  background-size: auto 100%;
+    background-image: url('../assets/kiminame.png');
+    background-size: auto 100%;
 }
 </style>
